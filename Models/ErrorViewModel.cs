c3fc770c1aa399f8d5acee2bc83bdc8f7c205f1e
everyline94 <brunolujanlarocca@gmail.com>
2024-09-05 13:51:46 -0300

    commit inicial do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Pim2024_FazendaUrbana.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
